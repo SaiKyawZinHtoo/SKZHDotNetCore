dc53ccf8130994994d6a5802094078b2e37f282e
shanlay <shanlay@DESKTOP-MVSU0HC>
2024-10-28 23:52:10 +0630

    feature: make clean code and call back code main

--- SKZHDotNetCore.ConsoleApp/Program.cs
+++ SKZHDotNetCore.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SKZHDotNetCore.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -15,60 +16,11 @@ Console.WriteLine("Hello, World!");
 
 //nuget package download ya mal
 
-string connectionString = "Data Source=DESKTOP-MVSU0HC;Initial Catalog=SKZHDotNet;User ID=sa;Password=sasa@123;";
-Console.WriteLine("Connection string: " + connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
-
-Console.WriteLine("Connection opening...");
-connection.Open();
-Console.WriteLine("Connection opened.");
-
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
-SqlCommand cmd = new SqlCommand(query, connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-//DataTable dt = new DataTable();
-//adapter.Fill(dt);
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogId"]);
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-    //    //Console.WriteLine(dr["DeleteFlag"]);
-}
-
-
-//foreach (DataRow dr in dt.Rows)
-//{
-//    Console.WriteLine(dr["BlogId"]);g
-//    Console.WriteLine(dr["BlogTitle"]);
-//    Console.WriteLine(dr["BlogAuthor"]);
-//    Console.WriteLine(dr["BlogContent"]);
-//    //Console.WriteLine(dr["DeleteFlag"]);
-//}
-
-Console.WriteLine("Connection closing...");
-connection.Close();
-Console.WriteLine("Connection closed.");
-
-//DataSet
-//DataTable
-//DataRow
-//DataColumn
-
-//foreach (DataRow dr in dt.Rows) 
-//{
-//    Console.WriteLine(dr["BlogId"]);
-//    Console.WriteLine(dr["BlogTitle"]);
-//    Console.WriteLine(dr["BlogAuthor"]);
-//    Console.WriteLine(dr["BlogContent"]);
-//    //Console.WriteLine(dr["DeleteFlag"]);
-//}
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+//adoDotNetExample.Edit();
+//adoDotNetExample.Update();
+adoDotNetExample.Delete();
 
 Console.ReadKey();
