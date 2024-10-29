2d92626e5b44d6a1ac9ef7a1c4a774f8d5931f52
shanlay <shanlay@DESKTOP-MVSU0HC>
2024-10-29 23:29:09 +0630

    feature: make new console app 2

---
+++ SKZHDotNetCore.ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SKZHDotNetCore.DataBase.Model;
+
+Console.WriteLine("Hello, World!");
+
+AppDbContext db = new AppDbContext();
+var lst = db.TblBlogs.ToList();
