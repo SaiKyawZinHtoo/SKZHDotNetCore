d2993a352c173c68a4398171b41e5cd27c9fd6ef
shanlay <shanlay@DESKTOP-MVSU0HC>
2024-10-31 02:12:33 +0630

    feature: adodotnetExample2 and dapperExample2 complete

---
+++ SKZHDotNetCore.ConsoleApp/AdoDotNetExample2.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Data.SqlClient;
+using SKZHDotNetCore.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKZHDotNetCore.ConsoleApp
+{
+    internal class AdoDotNetExample2
+    {
+        private readonly string _connectionString = "Data Source=DESKTOP-MVSU0HC;Initial Catalog=SKZHDotNet;User ID=sa;Password=sasa@123;";
+        private readonly AdoDotNetService _adoDotNetService;
+
+        public AdoDotNetExample2()
+        {
+            _adoDotNetService = new AdoDotNetService(_connectionString);
+        }
+
+        public void Read()
+        {
+            string query = @"SELECT [BlogId]
+                              ,[BlogTitle]
+                              ,[BlogAuthor]
+                              ,[BlogContent]
+                              ,[DeleteFlag]
+                          FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+            var dt = _adoDotNetService.Query(query);
+            foreach(DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(dr["BlogId"]);
+                Console.WriteLine(dr["BlogTitle"]);
+                Console.WriteLine(dr["BlogAuthor"]);
+                Console.WriteLine(dr["BlogContent"]);
+            }
+        }
+
+        public void Edit()
+        {
+            Console.WriteLine("Blog id: ");
+            string id = Console.ReadLine();
+
+            string query = @"SELECT [BlogId]
+                                  ,[BlogTitle]
+                                  ,[BlogAuthor]
+                                  ,[BlogContent]
+                                  ,[DeleteFlag]
+                              FROM [dbo].[Tbl_Blog] where BlogId =@BlogId";
+
+            var dt = _adoDotNetService.Query(query, new SqlParameterModel("@BlogId", id));
+
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine(dr["BlogId"]);
+            Console.WriteLine(dr["BlogTitle"]);
+            Console.WriteLine(dr["BlogAuthor"]);
+            Console.WriteLine(dr["BlogContent"]);
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Blog Content: ");
+            string content = Console.ReadLine();
+
+
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)";
+
+            int result = _adoDotNetService.Execute(query,
+                new SqlParameterModel("@BlogTitle", title),
+                new SqlParameterModel("@BlogAuthor", author),
+                new SqlParameterModel("@BlogContent", content));
+
+            Console.WriteLine(result == 1 ? "Saving Successful." : "Saving Faileds.");
+        }
+
+        //public void Delete()
+        //{
+        //    Console.WriteLine("Blog Id: ");
+        //    string blogId = Console.ReadLine();
+
+
+        //    string query = @"DELETE FROM [dbo].[Tbl_Blog]
+        //                    WHERE BlogId = @BlogId";
+
+        //    int result = _adoDotNetService.Execute(query, new SqlParameterModel("@BlogId", id));
+
+
+        //    Console.WriteLine(result == 1 ? "Delete Successful." : "Delete Faileds.");
+        //}
+    }
+}
---
+++ SKZHDotNetCore.ConsoleApp/DapperExample2.cs
@@ -0,0 +1,78 @@
+﻿using Dapper;
+using SKZHDotNetCore.ConsoleApp.Models;
+using SKZHDotNetCore.Shared;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKZHDotNetCore.ConsoleApp
+{
+    internal class DapperExample2
+    {
+        private readonly string _connectionString = "Data Source=DESKTOP-MVSU0HC;Initial Catalog=SKZHDotNet;User ID=sa;Password=sasa@123;";
+        private readonly DapperService _dapperService;
+
+        public DapperExample2()
+        {
+            _dapperService = new DapperService(_connectionString); 
+        }
+
+        public void Read()
+        {
+            string query = "select * from tbl_blog where DeleteFlag = 0;";
+            var lst = _dapperService.Query<BlogDapperDataModel>(query).ToList();
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            }
+        }
+
+        public void Edit(int id)
+        {
+                string query = "selected * from tbl_blog where DeleteFlag = 0 and BlogId = @BlogId;";
+                var item = _dapperService.QueryFirstOrDefault<BlogDapperDataModel>(query, new BlogDapperDataModel
+                {
+                    BlogId = id
+                });
+
+                if (item is null)
+                {
+                    Console.WriteLine("No Data Found.");
+                    return;
+                }
+
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);  
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+                           ([BlogTitle]
+                           ,[BlogAuthor]
+                           ,[BlogContent]
+                           ,[DeleteFlag])
+                     VALUES
+                           (@BlogTitle
+                           ,@BlogAuthor
+                           ,@BlogContent
+                           ,0)";
+
+                int result = _dapperService.Execute(query, new BlogDapperDataModel
+                {
+                    BlogTitle = title,
+                    BlogAuthor = author,
+                    BlogContent = content,
+                });
+                Console.WriteLine(result == 1 ? "Saving Successful." : "Saving Failed.");
+        }
+    }
+}
